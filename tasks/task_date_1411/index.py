3acb3717c76c803802b94a08a0f59cbf3800634e
Kihito <benner.andrey.loon@gmail.com>
2023-11-15 00:26:37 +0300

    task_1411

---
+++ tasks/task_date_1411/index.py
@@ -0,0 +1,37 @@
+import math
+
+class Learn1411:
+
+    def count_money(self, coins: list[int]):
+        count: int = sum(coins)
+        if(count == len(coins)):
+            print(f"0")
+        elif(count > (math.floor(len(coins) / 2))):
+            print(f"{len(coins) - count}")
+        else: 
+            print(f"{count}")
+
+    def xy(self, s: int, p: int):
+        number: str = ""
+
+        if(s > 1000 | p > 1000):
+            print("Numbers exceed the limit")
+
+        for i in range(s + 1):
+            for j in range(s + 1):
+                if(((i * j == p) & (i + j == s)) & (i <= j)):
+                    number += str(i) + " " + str(j) + " "
+
+        print(f"{number}")     
+
+    def sqrtTwo(self, n: int):
+        for i in range(n):
+            if((2 ** i) <= n):
+                print(f"{2 ** i}")
+
+
+learn: Learn1411 = Learn1411()
+
+learn.count_money([0, 1, 1, 1, 1])
+#learn.xy(s=12, p=27)
+#learn.sqrtTwo(n=256)
---
+++ tasks/task_date_2810/index.py
@@ -0,0 +1,45 @@
+import math
+class Learn2810:
+    def sumThreeNumber(self, number: str):
+        sum: int = 0
+        for i in number:
+            sum += int(i)
+        print(sum)
+
+    # ratios 1:1:4
+    def paperCranes(self, number: int):
+        correlationToOne: int = int(number / 6)
+        print(correlationToOne, correlationToOne * 4, correlationToOne)
+
+    def luckyTicket(self, number: str):
+        long: int = int(len(number))
+        leftNumber: str = number[0: int(long/2)]
+        rightNumber: str = number[int(long/2): long]
+
+        leftNumberSum: int = 0
+        rightNumberSum: int = 0
+        i: int = 0
+
+        if len(leftNumber) != len(rightNumber):
+            print("no")
+
+        while i < int(long/2):
+            leftNumberSum += int(leftNumber[i])
+            rightNumberSum += int(rightNumber[i])
+            i += 1
+
+        if leftNumberSum == rightNumberSum:
+            print("yes")
+        else:
+            print("no")
+
+    def splittingChocolateBar(self, a: int, b: int, c: int) -> None:
+        if(c > (a * b)):
+            print("no")
+        elif(c == 1 & ((a == 1) | (b == 1))):
+            print("yes")
+        elif (a != 1) & (b != 1) & (c != 1):
+            if (a % c == 0) | (c % a == 0) | (b % c == 0) | (c % b == 0):
+                print("yes")
+        else:
+            print("no")
