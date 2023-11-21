2a9762dbc5f57af907e45f13affdb5310abc1bcb
Kihito <benner.andrey.loon@gmail.com>
2023-11-21 21:27:11 +0300

    update

--- tasks/task_date_2111/index.py
+++ tasks/task_date_2111/index.py
@@ -21,18 +21,3 @@ class Learn2111:
 learn: Learn2111 = Learn2111()
 learn.ellerIntersection(st1='1 2 3', st2='1 2 3 4')
 #learn.maxBerries(arr=[5, 8, 10, 4, 9, 2, 7, 3])
-
-"""
-var2 = '1 2 3'
-var3 = '1 2 3 4'
-
-var2 = '5 6 7 8' 
-var3 = '6 7 8 9'
-
-var2 = '1'
-var3 = '3 4 5 6'
-
-var2 = '10 20 30 40 50'
-var3 = '10 20 30 40 50'
-
-"""
