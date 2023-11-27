2af3b0bd5b8e2a46742c2977594937205507b4ab
Kihito <benner.andrey.loon@gmail.com>
2023-11-27 17:19:33 +0300

    task 2711

---
+++ tasks/task_date_2711/index.py
@@ -0,0 +1,16 @@
+class Learn2711:
+    def pow(self, a: int, b: int):
+        if b == 1:
+            return a
+
+        return self.pow(a, b - 1) * a
+
+    def sum(self, a: int, b: int):
+        if b == 0: 
+            return a
+        return self.sum(a + 1, b - 1)
+
+learn: Learn2711 = Learn2711()
+
+print(learn.pow(2, 5))
+print(learn.sum(16, 6))
