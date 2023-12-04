b47bc33c5549e837f231cd8661d9785eb9e009ce
Kihito <benner.andrey.loon@gmail.com>
2023-12-05 01:19:20 +0300

    types and update

--- tasks/task_date_0112/index.py
+++ tasks/task_date_0112/index.py
@@ -1,5 +1,5 @@
 class Learn0112:
-    def print_operation_table(self, operation, num_rows, num_columns):
+    def print_operation_table(self, operation, num_rows, num_columns) -> None:
         if num_rows < 2 or num_columns < 2:
             print("ОШИБКА! Размерности таблицы должны быть больше 2!")
         else:
@@ -11,25 +11,25 @@ class Learn0112:
                 ]
                 print(" ".join(row))
 
-    def win_and_pooh(self, string: str) -> bool:
-        str: list = string.split()
+    def win_and_pooh(self, string: str) -> None:
+        string: list = string.split()
 
-        list_1 = []
-        for word in str:
-            sum_w = 0
-            for i in word:
-                if i in "аеёиоуыэюя":
-                    sum_w += 1
-            list_1.append(sum_w)
-
-        return len(list_1) == list_1.count(list_1[0])
+        if len(string) == 1:
+            print("none")
+        else:
+            lst: list[int] = []
+            for word in string:
+                sum_w = 0
+                for i in word:
+                    if i in "аеёиоуыэюя":
+                        sum_w += 1
+                lst.append(sum_w)
+            if len(lst) == lst.count(lst[0]):
+                print("Парам пам-пам")
+            else:
+                print("Пам парам")    
 
 
 learn: Learn0112 = Learn0112()
 # learn.print_operation_table(lambda x, y: x * y, 3, 3)
 isParam = learn.win_and_pooh("за-гад-ка-ра-свет-ка-ра-газ-да-не-на-ма-ли-ва-ла")
-
-if isParam:
-    print("Парам пам-пам")
-else:
-    print("Пам парам")
