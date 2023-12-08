c127ed3b72982b4430a108b38dd150ea25282a42
Kihito <benner.andrey.loon@gmail.com>
2023-12-08 14:47:02 +0300

    task 0812

---
+++ tasks/task_date_0812/index.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df: pd.DataFrame = pd.read_csv("c:/IT/PythonProject/python-learn/tasks/task_date_0812/california_housing_train.csv")
+avg: pd.DataFrame = df[df["population"] < 500]["median_house_value"].mean()
+
+print(avg)
+
+minPopulation: int = df["population"].min()
+max_households_in_min_population = df[df["population"] == minPopulation]["households"].max()
+
+print(max_households_in_min_population)
