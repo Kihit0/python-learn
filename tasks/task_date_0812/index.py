b5514ca0dd25e09ab4341f78858148870fd1cd61
Kihito <benner.andrey.loon@gmail.com>
2023-12-12 12:51:11 +0300

    update

--- tasks/task_date_0812/index.py
+++ tasks/task_date_0812/index.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df: pd.DataFrame = pd.read_csv("c:/IT/PythonProject/python-learn/tasks/task_date_0812/california_housing_train.csv")
-avg: pd.DataFrame = df[df["population"] < 500]["median_house_value"].mean()
+avg: float = df[df["population"] < 500]["median_house_value"].mean()
 
 print(avg)
 
